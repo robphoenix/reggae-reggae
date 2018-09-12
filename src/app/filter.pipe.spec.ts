dce37028aa74ab59d71dfe27ced147bec1646716
Rob Phoenix <rob@robphoenix.com>
2018-09-12 20:19:26 +0100

    Add further filter pipe tests

--- src/app/filter.pipe.spec.ts
+++ src/app/filter.pipe.spec.ts
@@ -2,6 +2,10 @@ import { Artist } from './artist';
 import { FilterArtistsPipe } from './filter.pipe';
 
 describe('FilterArtistsPipe', () => {
+  const artists: Artist[] = [
+    { id: 1, name: 'Lee Scratch Perry' },
+    { id: 2, name: 'Bob Marley' },
+  ];
   let pipe: FilterArtistsPipe;
 
   beforeEach(() => {
@@ -13,10 +17,16 @@ describe('FilterArtistsPipe', () => {
   });
 
   it('should return artists array if no search term', () => {
-    const artists: Artist[] = [
-      { id: 1, name: 'Lee Scratch Perry' },
-      { id: 2, name: 'Bob Marley' },
-    ];
     expect(pipe.transform(artists, '')).toEqual(artists);
+  });
+
+  it('should return empty array if search term not present', () => {
+    expect(pipe.transform(artists, 'Metallica')).toEqual([]);
+  });
+
+  it('should return filtered array', () => {
+    expect(pipe.transform(artists, 'Lee')).toEqual([
+      { id: 1, name: 'Lee Scratch Perry' },
+    ] as Artist[]);
   });
 });
